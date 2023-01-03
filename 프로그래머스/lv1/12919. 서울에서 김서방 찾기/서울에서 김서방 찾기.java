5cbf464f611fe8be51219d652abaa55f5e4cccae
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-01-03 22:31:00 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.85 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String[] seoul) {
+        for(int i = 0; i < seoul.length; i++) {
+            String person = seoul[i];
+            if(person.equals("Kim")) {
+                return "김서방은 " + i + "에 있다";
+            }
+        }
+        return "김서방을 찾을 수 없습니다.";
+    }
+}
