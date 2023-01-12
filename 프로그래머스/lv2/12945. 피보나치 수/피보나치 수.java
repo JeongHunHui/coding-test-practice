6cecc7cbae718ac6b27e71a0e9cc91375f9d672c
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-01-12 22:14:33 +0900

    [level 2] Title: 피보나치 수, Time: 1.60 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -1,24 +1,13 @@
 class Solution {
-    final int divideNum = 1234567;
     public int solution(int n) {
-        return fibo2(n);
-    }
-
-    public int fibo2(int n) {
-        double a = 0;
-        double b = 1;
-        double newNum = 0;
-        int count = 1;
+        int a = 0;
+        int b = 1;
+        int newNum = 0;
         for (int i = 2; i <= n; i++) {
-            newNum = a+b;
-            if(newNum >= divideNum) {
-                newNum = newNum % divideNum;
-                b = b % divideNum;
-                count++;
-            }
+            newNum = (a + b) % 1234567;
             a = b;
             b = newNum;
         }
-        return (int)(newNum);
+        return newNum;
     }
 }
