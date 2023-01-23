150fc4a238577b495d4ff76832942cfb76e52797
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-01-23 16:03:32 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -3,26 +3,19 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int roundNum = 0;
-        int num = 1;
-        while (n > num) {
+        for(int i = 1; n > i; i*=2) {
             roundNum++;
-            num *= 2;
         }
 
         for (int i = roundNum; i > 0; i--) {
             int middleNum = (int)Math.pow(2, i-1);
+            if((a > middleNum && b <= middleNum) || (a <= middleNum && b > middleNum)) return i;
             if(a > middleNum) {
-                if(b <= middleNum) return i;
-                a -= middleNum;
-                b -= middleNum;
-            }
-            else if(b > middleNum) {
-                if(a <= middleNum) return i;
                 a -= middleNum;
                 b -= middleNum;
             }
         }
-
+        
         return 0;
     }
 }
