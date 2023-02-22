3324ade7dc523cbb841871533d27308206b52a23
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-02-22 23:00:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.49 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,35 @@
 from collections import deque
 
 def solution(maps):
-    col = len(maps)
-    row = len(maps[0])
-    is_visited = [[0 for _ in range(0,row)] for _ in range(0,col)]
-    is_visited[0][0] = 1
+    col_count = len(maps)
+    row_count = len(maps[0])
+    col = 0
+    row = 0
+    col_move = [0, 0, 1, -1]
+    row_move = [1, -1, 0, 0]
+    is_visited = [[0 for _ in range(row_count)] for _ in range(col_count)]
     
     def bfs():
         q = deque()
         q.append(0)
         q.append(0)
         q.append(1)
-        dc = [0,0,1,-1]
-        dr = [1,-1,0,0]
+        
         while len(q) > 0:
             c = q.popleft()
             r = q.popleft()
-            depth = q.popleft()
-            if c == col-1 and r == row-1:
-                return depth
-            for i in range(0,4):
-                new_c = c+dc[i]
-                new_r = r+dr[i]
-                if new_c < 0 or new_c >= col or new_r < 0 or new_r >= row or is_visited[new_c][new_r] == 1 or maps[new_c][new_r] == 0:
+            d = q.popleft()
+            if c == col_count-1 and r == row_count-1:
+                return d
+            for i in range(len(col_move)):
+                new_col = c + col_move[i]
+                new_row = r + row_move[i]
+                if new_col >= col_count or new_col < 0 or new_row >= row_count or new_row < 0 or maps[new_col][new_row] == 0 or is_visited[new_col][new_row] == 1:
                     continue
-                q.append(new_c)
-                q.append(new_r)
-                q.append(depth+1)
-                is_visited[new_c][new_r] = 1
+                is_visited[new_col][new_row] = 1
+                q.append(new_col)
+                q.append(new_row)
+                q.append(d+1)
         return -1
     
     return bfs()
