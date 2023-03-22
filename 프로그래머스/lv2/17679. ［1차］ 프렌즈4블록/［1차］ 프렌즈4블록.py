b6885aea1a575da2b1c778be70e569641ca1885e
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-03-23 02:41:02 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 12.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -2,31 +2,26 @@ def solution(m, n, board):
     temp_board = [[c2 for c2 in c] for c in board]
     main_board = [[c2 for c2 in c] for c in board]
     
-    def check_rect(r,c):
+    def get_rect_set(r,c):
         block = main_board[r][c]
-        new_set = set()
-        if r+1 >= m or c+1 >= n or block == ' ': return new_set
-        if block == main_board[r+1][c] and block == main_board[r][c+1] and block == main_board[r+1][c+1]:
-            new_set.add((r,c))
-            new_set.add((r+1,c))
-            new_set.add((r,c+1))
-            new_set.add((r+1,c+1))
-            return new_set
-        else: return new_set
+        if r+1 >= m or c+1 >= n or block == ' ':
+            return set()
+        return set([(r,c),(r+1,c),(r,c+1),(r+1,c+1)]) if block == main_board[r+1][c] == main_board[r][c+1] == main_board[r+1][c+1] else set()
     
-    is_changed = True
-    count = 0
-    while is_changed:
+    while True:
         new_set = set()
         for r in range(m):
             for c in range(n):
-                new_set = new_set | check_rect(r, c)
-        if len(new_set) == 0: break
+                new_set = new_set | get_rect_set(r, c)
+                
+        if len(new_set) == 0:
+            break
+            
         for s in new_set:
             temp_board[s[0]][s[1]] = ' '
             
-        for c in range(len(temp_board[0])):
-            for r in range(len(temp_board)-2,-1,-1):
+        for c in range(m):
+            for r in range(n-2,-1,-1):
                 for i in range(r, len(temp_board)-1):
                     down_block = temp_board[i+1][c]
                     if down_block == ' ':
@@ -35,9 +30,4 @@ def solution(m, n, board):
         
         main_board = temp_board
 
-    answer = 0
-    for board in temp_board:
-        for block in board:
-            if block == ' ':
-                answer += 1
-    return answer
+    return sum([board.count(' ') for board in temp_board])
