65c477cb048ca37441a418e37afee49e0c476b5b
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-08-08 23:26:15 +0900

    [level 2] Title: 올바른 괄호, Time: 6.69 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,10 @@
+def solution(s):
+    count = 0
+    for c in s:
+        if c == '(':
+            count += 1
+        elif c == ')':
+            count -= 1
+            if count < 0:
+                return False
+    return count == 0
