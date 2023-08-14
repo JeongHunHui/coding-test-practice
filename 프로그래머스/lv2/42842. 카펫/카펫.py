0e841b70d840898c532ffc8aa289b23caf106682
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-08-15 00:21:59 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,4 +1,4 @@
 def solution(brown, yellow):
-    for i in range(1,int(yellow**0.5)+1):
-        if yellow % i == 0 and (i + 2 + yellow//i) * 2 == brown:
-            return [yellow//i+2, i+2]
+    for i in range(1, int(yellow**0.5) + 1):
+        if yellow % i == 0 and (yellow / i + 2) * 2 + i * 2 == brown:
+            return [yellow / i + 2, i + 2]
