46f5482219b1f37cb8b0991a693dd3651a555767
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-08-17 23:03:16 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.68 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,9 +1,8 @@
 def solution(arr1, arr2):
-    a = len(arr1)
-    b = len(arr2[0])
-    answer = [[0 for _ in range(b)] for _ in range(a)]
-    for i in range(a):
-        for j in range(b):
-            for k in range(len(arr1[0])):
-                answer[i][j] += arr1[i][k] * arr2[k][j]
-    return answer
+    result = [[0 for _ in range(len(arr2[0]))] for _ in range(len(arr1))]
+
+    for i in range(len(arr1)):
+        for j in range(len(arr2[0])):
+            for k in range(len(arr2)):
+                result[i][j] += arr1[i][k] * arr2[k][j]
+    return result
