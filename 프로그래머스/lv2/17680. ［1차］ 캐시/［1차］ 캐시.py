41ef2e230da7ebbfb9bec0caf869667664285067
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-08-18 20:47:33 +0900

    [level 2] Title: [1차] 캐시, Time: 0.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -3,14 +3,12 @@ from collections import deque
 def solution(cacheSize, cities):
     if cacheSize == 0:
         return len(cities) * 5
-    cache = deque([])
+    cache = deque(maxlen = cacheSize)
     cache_hit_count = 0
     for city in cities:
         city = city.lower()
         if city in cache:
             cache_hit_count += 1
             cache.remove(city)
-        elif len(cache) >= cacheSize:
-            cache.popleft()
         cache.append(city)
     return (len(cities) - cache_hit_count) * 5 + cache_hit_count
