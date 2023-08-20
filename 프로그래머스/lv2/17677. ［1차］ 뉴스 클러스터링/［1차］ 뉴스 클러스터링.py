d7bd95ce6aed9b599c1839c9b68235d5939720ba
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-08-21 01:07:40 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -5,15 +5,11 @@ const_num = 65536
 
 def make_set(s):
     s = s.lower()
-    s_counter = Counter([s[i:i+2] for i in range(len(s)-1)])
-    s_counter = Counter({k: v for k, v in s_counter.items() if k.isalpha()})
-    return s_counter
+    return Counter([s[i:i+2] for i in range(len(s)-1) if s[i:i+2].isalpha()])
 
 def solution(str1, str2):
     sets_1 = make_set(str1)
     sets_2 = make_set(str2)
     count_1 = sum((sets_1 & sets_2).values())
     count_2 = sum((sets_1 | sets_2).values())
-    if count_2 == 0:
-        return const_num
-    return math.floor(count_1 / count_2 * const_num)
+    return math.floor(count_1 / count_2 * const_num) if count_2 > 0 else const_num
