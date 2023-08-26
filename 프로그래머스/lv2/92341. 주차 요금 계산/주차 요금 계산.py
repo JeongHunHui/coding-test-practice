1e37601e604946d774d22fb6356c56dbd52fca69
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-08-27 01:17:49 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,34 +1,44 @@
 import math
 
+out = 'OUT'
+
 def get_time(start, finish):
     start_hour, start_min = map(int, start.split(':'))
     finish_hour, finish_min = map(int, finish.split(':'))
     return 60 * (finish_hour - start_hour) + finish_min - start_min
 
-def solution(fees, records):
-    basic_time, basic_fee, time_term, term_fee = fees
-    def calculate_fee(time):
-        left_time = time - basic_time
-        return basic_fee + math.ceil(left_time / time_term) * term_fee if left_time > 0 else basic_fee
+def calculate_fee(fee_info, time):
+    basic_time, basic_fee, time_term, term_fee = fee_info
+    left_time = time - basic_time
+    if left_time > 0:
+        return basic_fee + math.ceil(left_time / time_term) * term_fee
+    return basic_fee
+
+def make_car_info_dict(records):
     car_dict = {}
     for record in records:
         time, num, in_out = record.split()
-        is_in = in_out == 'IN'
-        if is_in:
-            if num in car_dict:
-                car_dict[num][0] = time
-            else:
-                car_dict[num] = [time, 0]
-        else:
+        if in_out == out:
             start_time, time_sum = car_dict[num]
-            car_dict[num] = [None, time_sum + get_time(start_time, time)]
-    car_nums = sorted(car_dict.keys())
-    for num in car_nums:
+            time_sum += get_time(start_time, time)
+            car_dict[num] = [None, time_sum]
+        elif num in car_dict:
+            car_dict[num][0] = time
+        else:
+            car_dict[num] = [time, 0]
+    
+    for num in car_dict.keys():
         start_time, time_sum = car_dict[num]
         if start_time != None:
             time_sum += get_time(start_time, '23:59')
             car_dict[num] = [None, time_sum]
-    return [calculate_fee(car_dict[i][1]) for i in car_nums]
+    
+    return car_dict
 
-    
-    
+def make_fee_list_by_car_dict(car_dict, fee_info):
+    car_nums = sorted(car_dict.keys())
+    return [calculate_fee(fee_info, car_dict[num][1]) for num in car_nums]
+
+def solution(fees, records):
+    car_dict = make_car_info_dict(records)
+    return make_fee_list_by_car_dict(car_dict, fees)
