409bcf5845e8ea92e80209e53fac820c5da600e9
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-08-29 20:58:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.66 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,35 +1,29 @@
 from collections import deque
 
+def get_move_pos_list(x, y):
+    return [(x,y-1), (x,y+1), (x-1,y), (x+1,y)]
+
 def solution(maps):
-    col_count = len(maps)
-    row_count = len(maps[0])
-    col = 0
-    row = 0
-    col_move = [0, 0, 1, -1]
-    row_move = [1, -1, 0, 0]
-    is_visited = [[0 for _ in range(row_count)] for _ in range(col_count)]
+    row = len(maps[0])
+    col = len(maps)
     
-    def bfs():
-        q = deque()
-        q.append(0)
-        q.append(0)
-        q.append(1)
+    visited = [[False for _ in range(row)] for _ in range(col)]
+    
+    def can_visit(x, y, count):
+        return x >= 0 and x < row and y >= 0 and y < col and not visited[y][x] and (maps[y][x] == 1 or maps[y][x] == count)
+    
+    def is_opposite(x, y):
+        return x == row-1 and y == col-1
         
-        while len(q) > 0:
-            c = q.popleft()
-            r = q.popleft()
-            d = q.popleft()
-            if c == col_count-1 and r == row_count-1:
-                return d
-            for i in range(len(col_move)):
-                new_col = c + col_move[i]
-                new_row = r + row_move[i]
-                if new_col >= col_count or new_col < 0 or new_row >= row_count or new_row < 0 or maps[new_col][new_row] == 0 or is_visited[new_col][new_row] == 1:
-                    continue
-                is_visited[new_col][new_row] = 1
-                q.append(new_col)
-                q.append(new_row)
-                q.append(d+1)
-        return -1
-    
-    return bfs()
+    queue = deque([(0,0,2)])
+    while queue:
+        x, y, count = queue.popleft()
+        move_pos_list = get_move_pos_list(x, y)
+        for new_x, new_y in move_pos_list:
+            if can_visit(new_x, new_y, count):
+                if is_opposite(new_x, new_y):
+                    return count
+                maps[new_y][new_x] = count + 1
+                visited[new_y][new_x] = True
+                queue.append((new_x, new_y, count+1))
+    return -1
