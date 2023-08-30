4b4d098417bd83e56b45014114ea0df45048b4ad
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-08-30 22:16:09 +0900

    [level 2] Title: 주식가격, Time: 67.60 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,10 +1,12 @@
 def solution(prices):
+    price_len = len(prices)
     answer = []
-    for i in range(len(prices)):
-        current_num = prices[i]
-        count = 0
-        for j in range(i+1, len(prices)):
-            count += 1
-            if prices[j] < current_num : break
-        answer.append(count)
+    for i in range(price_len):
+        current_price = prices[i]
+        sec = 0
+        for j in range(i+1, price_len):
+            sec += 1
+            if current_price > prices[j]:
+                break
+        answer.append(sec)
     return answer
