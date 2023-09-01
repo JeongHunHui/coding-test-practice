839307dcf1c3f29f8da3f8ad067cdf3fdc10cd99
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-09-02 04:27:59 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,16 +1,7 @@
-def is_tree_correct(skill, my_skill):
-    for i in range(len(my_skill)):
-        if skill[i] != my_skill[i]:
-            return False
-    return True
-
 def solution(skill, skill_trees):
     answer = 0
-    for skill_tree in skill_trees:
-        my_skill = ""
-        for s in skill_tree:
-            if s in skill:
-                my_skill += s
-        if is_tree_correct(skill, my_skill):
+    for tree in skill_trees:
+        new_tree = ''.join([c for c in tree if c in skill])
+        if new_tree == skill[:len(new_tree)]:
             answer += 1
     return answer
