49ae77857280ab3eadb310ad0eac81b93df5dba6
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-09-22 17:42:16 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.34 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,27 +1,31 @@
 from itertools import permutations
 import re
 
+def calc_op(n1,n2,op):
+    if op == '*':
+        return n1 * n2
+    if op == '+':
+        return n1 + n2
+    if op == '-':
+        return n1 - n2
+
+def calc(e, ops):
+    for op in ops:
+        i = 0
+        while i < len(e):
+            if op != e[i]:
+                i += 1
+                continue
+            e[i-1] = calc_op(e[i-1], e[i+1], op)
+            e = e[:i] + e[i+2:]
+    return abs(int(e[0]))
+
 def solution(expression):
-    answer = 0
-    operators_list = list(permutations('*+-',3))
+    # 모든 연산자 우선순위 순서 만들기
+    ops_list = list(permutations('*+-',3))
+    # 숫자, 연산자를 고르고 숫자는 int로 형변환
     elements = re.findall(r'\d+|[*+-]', expression)
     for i in range(0,len(elements),2):
         elements[i] = int(elements[i])
-    for op in operators_list:
-        n_elements = elements[:]
-        for o in op:
-            i = 0
-            while i < len(n_elements):
-                e = n_elements[i]
-                if o == e:
-                    if o == '*':
-                        n_elements[i-1] *= n_elements[i+1]
-                    elif o == '+':
-                        n_elements[i-1] += n_elements[i+1]
-                    elif o == '-':
-                        n_elements[i-1] -= n_elements[i+1]
-                    n_elements = n_elements[:i] + n_elements[i+2:]
-                else:
-                    i += 1
-        answer = max(answer, abs(int(n_elements[0])))
-    return answer
+    # 각 요소들을 연산자 우선순위들로 계산한 값 중 최대값을 반환
+    return max(calc(elements[:], ops) for ops in ops_list)
