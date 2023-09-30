5aa641e530bae51e2365cad4b519488451a172ed
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-09-30 17:34:38 +0900

    [Silver III] Title: N과 M (5), Time: 116 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,20 @@
+n,m=list(map(int, input().split()))
+nums=sorted(list(map(int, input().split())))
+answer = []
+is_visited = [False]*n
+
+def backtracking(depth):
+  # 탈출 조건: m개의 숫자를 찾으면 종료
+  if depth == m:
+    print(' '.join(answer))
+    return
+  # 가지 치기: 탐색한 답 추가 후 재귀호출
+  for i in range(n):
+    if not is_visited[i]:
+      is_visited[i] = True
+      answer.append(str(nums[i]))
+      backtracking(depth+1)
+      answer.pop()
+      is_visited[i] = False
+
+backtracking(0)
