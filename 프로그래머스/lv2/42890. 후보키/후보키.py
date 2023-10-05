f69edceeaa40388563dee38f0a3b9dc204c9a6b7
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-10-05 14:27:47 +0900

    [level 2] Title: 후보키, Time: 0.64 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -1,17 +1,3 @@
-from itertools import combinations as comb
 def solution(relation):
-    colCount = len(relation[0])
-    rowCount = len(relation)
-    cKeys = set()
-    for i in range(colCount):
-        combs = [c for c in comb(range(colCount),i+1) if all(not set(key).issubset(set(c)) for key in cKeys)]
-        for c in combs:
-            checkSet = set()
-            for row in relation:
-                arr = []
-                for i in c:
-                    arr.append(row[i])
-                checkSet.add(tuple(arr))
-            if len(checkSet) == rowCount:
-                cKeys.add(c)
-    return len(cKeys)
+    answer = 0
+    return answer
