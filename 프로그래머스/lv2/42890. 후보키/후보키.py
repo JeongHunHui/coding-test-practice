8e862aca1b707dc54409c94ee887c35c7cf523e1
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-10-05 14:38:58 +0900

    [level 2] Title: 후보키, Time: 0.66 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -1,3 +1,21 @@
+from itertools import combinations as comb
+
 def solution(relation):
-    answer = 0
-    return answer
+    # 컬럼 개수, 레코드 수
+    colCount, rowCount = len(relation[0]), len(relation)
+    # 후보키 집합
+    cKeys = set()
+    for i in range(colCount):
+        # 튜플 조합 생성
+        combs = comb(range(colCount),i+1)
+        # 튜플 조합 중 후보키가 포함되는 조합 제거
+        combs = [c for c in combs if all(not set(key).issubset(set(c)) for key in cKeys)]
+        # 조합들을 반복을 돌며 각 조합이 후보키인지 체크
+        for c in combs:
+            checkSet = set()
+            for row in relation:
+                checkSet.add(tuple(row[i] for i in c))
+            if len(checkSet) == rowCount:
+                cKeys.add(c)
+    # 후보키의 개수를 반환
+    return len(cKeys)
