afec1945fcd1bd9080ef23413a07bb3aa83ffa37
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-10-05 16:43:14 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,21 +1,23 @@
 def compression(s, n):
-    answer = ''
-    count = 0
-    cStr = s[0:n]
-    temp = ''
+    # 누적 글자 길이, 현재 압축 횟수, 현재 압축 문자열
+    answer, count, cStr = 0, 0, s[0:n]
     for i in range(0,len(s),n):
-        temp = s[i:i+n]
-        if cStr == temp:
+        # 현재 압축 중인 문자열과 같으면 카운트 증가
+        if cStr == s[i:i+n]:
             count += 1
         else:
-            answer += f'{count if count != 1 else ""}{cStr}'
-            cStr = temp
+            # 누적 글자 길이 증가(글자 길이 + 숫자 길이)
+            answer += len(str(count))+n if count != 1 else n
+            # 현재 압축 문자열 초기화
+            cStr = s[i:i+n]
+            # 현재 압축 횟수 초기화
             count = 1
-    return len(answer + f'{count if count != 1 else ""}{cStr}')
+    # 남은 문자열 만큼 누적 글자 길이 증가
+    return answer + (len(str(count)+cStr) if count != 1 else len(cStr))
 
 def solution(s):
-    l = len(s)
-    answer = l
-    for n in range(1,l):
+    answer = len(s)
+    # 모든 글자 길이 만큼 압축하며 최소 글자 길이 구해서 반환
+    for n in range(1,len(s)):
         answer = min(answer, compression(s, n))
     return answer
