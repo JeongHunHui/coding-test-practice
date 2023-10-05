34a34e3c5390bfc01062c40e45ebef60e8975f41
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-10-05 15:47:29 +0900

    [level 2] Title: [3차] n진수 게임, Time: 9.43 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,25 +1,19 @@
-import string
-
-num_string = string.digits + string.ascii_uppercase
-
-def convert_num(num, base):
-    result = ""
-    while num > 0:
-        result += num_string[num % base]
-        num //= base
-    return result[::-1] if result != "" else '0'
+def baseConv(num, base):
+    result, nums = '', '0123456789ABCDEF'
+    while num:
+        num, r = divmod(num, base)
+        result = nums[r] + result
+    return result if result != '' else '0'
 
 def solution(n, t, m, p):
-    current_num = 0
-    current_seq = 1
-    answer = ""
-    while True:
-        converted_num_str = convert_num(current_num, n)
-        for c in converted_num_str:
-            if current_seq == p:
-                answer += c
-            current_seq = current_seq % m + 1
-            if len(answer) >= t:
-                return answer
-        current_num += 1
-    return -1
+    answer, cNum, num, cCount, i = '', '0', 0, 0, 0
+    while len(answer) < t:
+        if i%m+1 == p:
+            answer += cNum[cCount]
+        i += 1
+        cCount += 1
+        if cCount == len(cNum):
+            num += 1
+            cNum = baseConv(num, n)
+            cCount = 0
+    return answer
