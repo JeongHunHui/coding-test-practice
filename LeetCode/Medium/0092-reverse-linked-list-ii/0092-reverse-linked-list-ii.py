8363fc3695e764ecc514e8c3d388d80d04b3ed79
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-11-21 02:43:09 +0900

    [Medium] Title: 92. Reverse Linked List II, Time: 33 ms (90.09%), Memory: 16.5 MB ((11.37)%) - LeetHub

--- LeetCode/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ LeetCode/Medium/0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -1,19 +1,22 @@
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        valuesLeft = []
-        valuesMid = []
-        valuesRight = []
+        values_left = []
+        values_mid = []
+        values_right = []
         index = 1
+        # left 이전 노드는 values_left, 중간은 mid, right 이후 노드는 right에 넣는다.
         while head:
             if index < left:
-                valuesLeft.append(head.val)
+                values_left.append(head.val)
             elif index > right:
-                valuesRight.append(head.val)
+                values_right.append(head.val)
             else:
-                valuesMid.append(head.val)
+                values_mid.append(head.val)
             head = head.next
             index += 1
-        values = valuesRight[::-1] + valuesMid + valuesLeft[::-1]
+        # 링크드 리스트를 만들기위해 거꾸로 배열을 이어 붙힘
+        values = values_right[::-1] + values_mid + values_left[::-1]
+        # 마지막 노드부터 연결
         answer = ListNode(values[0], None)
         for val in values[1:]:
             answer = ListNode(val, answer)
