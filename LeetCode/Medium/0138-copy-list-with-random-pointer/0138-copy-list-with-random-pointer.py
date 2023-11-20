278cdacf2917cceec8284779a423f5b21af27dfd
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-11-21 02:18:56 +0900

    [Medium] Title: 138. Copy List with Random Pointer, Time: 48 ms (22.03%), Memory: 17.3 MB ((44.02)%) - LeetHub

--- LeetCode/Medium/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ LeetCode/Medium/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -2,14 +2,16 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
-        nodeDict = {}
-        currentNode = head
-        while currentNode:
-            nodeDict[currentNode] = Node(currentNode.val)
-            currentNode = currentNode.next
-        currentNode = head
-        while currentNode:
-            nodeDict[currentNode].next = nodeDict.get(currentNode.next)
-            nodeDict[currentNode].random = nodeDict.get(currentNode.random)
-            currentNode = currentNode.next
-        return nodeDict[head]
+        node_dict = {}
+        current_node = head
+        # 반복문을 돌며 딕셔너리에 원래 노드를 키로하고, 새로 만든 노드를 value로 하여 넣는다.
+        while current_node:
+            node_dict[current_node] = Node(current_node.val)
+            current_node = current_node.next
+        current_node = head
+        # 반복문을 돌며 새로 만든 노드의 next 노드와 random 노드를 설정한다.
+        while current_node:
+            node_dict[current_node].next = node_dict.get(current_node.next)
+            node_dict[current_node].random = node_dict.get(current_node.random)
+            current_node = current_node.next
+        return node_dict[head]
