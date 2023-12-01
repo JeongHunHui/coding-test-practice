3e15791358054c443758df1c1368448fea144f32
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-12-01 17:50:13 +0900

    [level 3] Title: 네트워크, Time: 0.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,14 +1,17 @@
 def solution(n, computers):
-    def dfs(y):
-        computers[y][y] = 2
-        for x in range(n):
-            if computers[y][x] == 1:
-                computers[y][x] = 2
-                computers[x][y] = 2
-                dfs(x)
-    answer = 0
-    for y in range(n):
-        if computers[y][y] == 1:
-            answer += 1
-            dfs(y)
-    return answer
+    def dfs(i):
+        # 방문 표시
+        computers[i][i] = 2
+        # i번 노드에서 갈 수 있는 다른 노드 탐색
+        for j in range(n):
+            if computers[i][j] == 1:
+                computers[i][j] = 2
+                computers[j][i] = 2
+                dfs(j)
+    count = 0
+    for i in range(n):
+        # 아직 방문하지 않은 노드 방문 시 count 증가, 해당 노드 탐색
+        if computers[i][i] == 1:
+            count += 1
+            dfs(i)
+    return count
