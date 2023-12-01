40480a6e8097fa47043af4ab64a87d31f214a43b
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-12-01 23:16:48 +0900

    [level 3] Title: 가장 먼 노드, Time: 103.16 ms, Memory: 24.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,47 +1,26 @@
+from collections import defaultdict
 import math, heapq
 
 def solution(n, edge):
-    node_dict = {i:[] for i in range(n)}
+    node_dict = defaultdict(list)
     for i, j in edge:
-        node_dict[i-1].append((j-1,1))
-        node_dict[j-1].append((i-1,1))
+        node_dict[i-1].append(j-1)
+        node_dict[j-1].append(i-1)
     
-    # start에서 다른 노드들로 가는 최단 거리들을 list로 반환
     def dijkstra(start):
-        start -= 1
-        heap = []
-        heapq.heappush(heap, (0, start))
-        # start에서 0 ~ n-1 노드까지 이동거리
-        # 우선 무한대로 선언한 뒤 탐색을 진행하며 최솟값으로 업데이트
-        distances = [math.inf] * n
-        # start에서 start까지는 거리가 0
+        # 1번 노드에서 각 노드까지의 최단경로를 저장할 list
+        distances = [math.inf]*n
         distances[start] = 0
-        
+        heap = []
+        heapq.heappush(heap, (start, 0))
         while heap:
-            distance, node = heapq.heappop(heap)
-            # node와 연결된 다른 노드(new_node)로 가는 경로 탐색
-            for new_node, d in node_dict[node]:
-                # start~node의 거리가 현재 최단 경로보다 크거나 같으면 스킵
-                if distance >= distances[new_node]:
-                    continue
-                # start~node + node~new_node
-                new_distance = distance + d
-                # 새로운 경로가 현재 최단 경로보다 작으면 업데이트, heap에 push
-                if new_distance < distances[new_node]:
-                    distances[new_node] = new_distance
-                    heapq.heappush(heap, (new_distance, new_node))
+            node, weight = heapq.heappop(heap)
+            for i in node_dict[node]:
+                if distances[i] > weight + 1:
+                    distances[i] = weight + 1
+                    heapq.heappush(heap, (i, weight+1))
         return distances
             
-    distances = dijkstra(1)
-    
-    max_dist = 0
-    answer = 0
-    for dist in distances:
-        if dist == max_dist:
-            answer += 1
-            continue
-        if dist > max_dist:
-            answer = 1
-            max_dist = dist
-        
-    return answer
+    distances = dijkstra(0)
+    max_dist = max(distances)
+    return sum([1 for d in distances if max_dist == d])
