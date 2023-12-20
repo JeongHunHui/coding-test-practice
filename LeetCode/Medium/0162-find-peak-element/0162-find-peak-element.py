2df9ae8ed8a8f99d6591fa8db9053b97b24a1ca5
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2023-12-20 23:57:43 +0900

    [Medium] Title: 162. Find Peak Element, Time: 50 ms (67.94%), Memory: 16.2 MB ((99.55)%) - LeetHub

--- LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.py
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.py
@@ -3,10 +3,8 @@ class Solution:
         left, right = 0, len(nums)-1
         while left < right:
             mid = (left + right) // 2
-            if nums[mid] < nums[mid + 1]:
-                # 피크는 오른쪽에 있을 수 있음
+            if nums[mid] < nums[mid+1]:
                 left = mid + 1
             else:
-                # 피크는 왼쪽에 있을 수 있음
                 right = mid
         return left
