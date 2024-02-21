f40c33108be66805ade771e4819f4fdaeb7bf63f
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2024-02-21 03:05:43 -0800

    [Silver I] Title: 영역 구하기, Time: 152 ms, Memory: 245136 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,33 +1,28 @@
-# 12:08
-from collections import deque
+# 02:45
+import sys
+sys.setrecursionlimit(100000)
 m, n, k = map(int, input().split())
-grid = [[True]*n for _ in range(m)]
+grid = [[0] * n for _ in range(m)]
 for _ in range(k):
     x1, y1, x2, y2 = map(int, input().split())
-    for y in range(y1, y2):
-        for x in range(x1, x2):
-            grid[y][x] = False
-
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            grid[y][x] = 1
 dirs = [(0,1),(0,-1),(1,0),(-1,0)]
-def bfs(x, y):
-    que = deque([(x, y)])
-    grid[y][x] = False
-    count = 0
-    while que:
-        x, y = que.popleft()
-        count += 1
-        for dx, dy in dirs:
-            nx, ny = x + dx, y + dy
-            if nx >= 0 and nx < n and ny >= 0 and ny < m and grid[ny][nx]:
-                grid[ny][nx] = False
-                que.append((nx, ny))
-    return count
-counts = []
+is_visited = [[False] * n for _ in range(m)]
+def dfs(x, y, is_visited, total):
+    is_visited[y][x] = True
+    total += 1
+    for dx, dy in dirs:
+        nx, ny = x+dx, y+dy
+        if nx >= 0 and nx < n and ny >= 0 and ny < m and not is_visited[ny][nx] and grid[ny][nx] == 0:
+            total += dfs(nx,ny,is_visited,0)
+    return total
+answers = []
 for y in range(m):
     for x in range(n):
-        if grid[y][x]:
-            count = bfs(x,y)
-            if count > 0:
-                counts.append(count)
-print(len(counts))
-print(' '.join(map(str, sorted(counts))))
+        if not is_visited[y][x] and grid[y][x] == 0:
+            answers.append(dfs(x,y,is_visited,0))
+answers.sort()
+print(len(answers))
+print(' '.join(map(str, answers)))
