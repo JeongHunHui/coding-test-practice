9a4b200e8786d5827fb5d24ef84b549e5cb1952a
정훈희 <108508730+JeongHunHui@users.noreply.github.com>
2024-03-02 13:37:51 +0900

    [level 3] Title: 정수 삼각형, Time: 36.00 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,12 +1,8 @@
-# 17:15
 def solution(triangle):
-    height = len(triangle)
-    answers = [[-1] * len(values) for values in triangle]
-    answers[0][0] = triangle[0][0]
-    for depth in range(1, height):
-        for i, val in enumerate(triangle[depth]):
-            left_parent = 0 if i == 0 else answers[depth-1][i-1]
-            right_parent = 0 if i == depth else answers[depth-1][i]
-            answers[depth][i] = max(left_parent, right_parent) + val
-    return max(answers[height-1])
-            
+    l = len(triangle)
+    for h in range(1, l):
+        triangle[h][0] += triangle[h-1][0]
+        for i in range(1, h):
+            triangle[h][i] += max(triangle[h-1][i-1], triangle[h-1][i])
+        triangle[h][h] += triangle[h-1][h-1]
+    return max(triangle[l-1])
